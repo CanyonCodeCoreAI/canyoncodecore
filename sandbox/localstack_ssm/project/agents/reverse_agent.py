e65a4cc8f98a4bd1347b67d51149ecbf1b4176f0
Saak <saakethsodanapalli@gmail.com>
2026-07-08 13:24:59 -0400

    removed folder that should be gitignored

--- sandbox/localstack_ssm/project/agents/reverse_agent.py
+++
@@ -1,6 +0,0 @@
-class ReverseAgent(object):
-    def __init__(self):
-        self.tools = [self.reverse]
-
-    def reverse(self, text: str) -> str:
-        return text[::-1]
--- sandbox/localstack_ssm/project/agents/smoke_agent.py
+++
@@ -1,6 +0,0 @@
-class SmokeAgent(object):
-    def __init__(self):
-        self.tools = [self.ping]
-
-    def ping(self, name: str) -> str:
-        return f"smoke:{name}"
