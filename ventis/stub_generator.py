53911f2e956571fd67a62277f2b1d4d7ab505119
Saaketh Sodanapalli <saaketh.s@canyoncode.ai>
2026-07-27 11:06:38 -0700

    Switched ventis build from building sequentially to in parallel with docker buildx bake

--- ventis/stub_generator.py
+++ ventis/stub_generator.py
@@ -358,7 +358,7 @@ ENV VENTIS_AGENT_FILE={agent_basename}
 
 EXPOSE 50051
 
-CMD python local_controller.py --port 50051
+CMD ["python", "local_controller.py", "--port", "50051"]
 """
     with open(os.path.join(output_dir, "Dockerfile"), "w") as f:
         f.write(dockerfile)
@@ -473,7 +473,7 @@ COPY . .
 EXPOSE 50051
 EXPOSE {api_port}
 
-CMD python workflow_launcher.py
+CMD ["python", "workflow_launcher.py"]
 """
     with open(os.path.join(output_dir, "Dockerfile"), "w") as f:
         f.write(dockerfile)
