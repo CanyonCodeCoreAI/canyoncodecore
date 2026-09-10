993e889f12dbfc2c694a8f8fc18f95a513a38443
Saaketh Sodanapalli <saaketh.s@canyoncode.ai>
2026-09-10 12:02:12 -0700

    Fixed Issue #10

--- canyonos_core/controller/local_controller.py
+++ canyonos_core/controller/local_controller.py
@@ -467,7 +467,7 @@ class LocalController(object):
             for key, value in args.items():
                 if (
                     isinstance(value, str)
-                    and len(value) == 32
+                    and len(value) == 16
                     and all(c in "0123456789abcdefABCDEF" for c in value)
                 ):
                     future_key = f"future:{value}"
@@ -531,7 +531,7 @@ class LocalController(object):
             # Check if this arg value is a UUID hex string identifying a future
             if (
                 isinstance(value, str)
-                and len(value) == 32
+                and len(value) == 16
                 and all(c in "0123456789abcdefABCDEF" for c in value)
             ):
                 future_key = f"future:{value}"
